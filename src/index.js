1f096c22019a606f51db73307bf71a01b9fd3c1c
Approxomiron <nzubtsov194@gmail.com>
2020-10-02 22:25:14 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array==undefined){
+    return 0
+  }else if(array.length==0){
+    return 0
+  }
+  else{
+    let len = array.length  
+    let min = Infinity
+    while (len--){
+      if(array[len] < min){
+        min = array[len]
+      }
+    }
+    
+    return min
+  }
+ 
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==undefined){
+    return 0
+  }else if (array.length==0){
+    return 0
+  } else {
+    let len = array.length  
+    let max = -Infinity
+    while (len--){
+      if(array[len] > max){
+        max = array[len]
+      }
+    }
+    
+    return max
+  }
+  
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if(array==undefined){
+    return 0
+  }else if(array.length==0){
+    return 0
+  }
+  else{
+    
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
+  
 }
+
